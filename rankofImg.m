7f9506835bb24d9e42c9751a00c8103da18f07b6
Sahil Gawande <66516087+saga0p@users.noreply.github.com>
2023-03-21 11:06:50 +0530

    Update rankofImg.m
    adding a homogeneous image below the original image

--- rankofImg.m
+++ rankofImg.m
@@ -2,3 +2,11 @@ A = imread('black.jfif');
 A = rgb2gray(A);
 imshow(A)
 title(['Original (',sprintf('Rank %d)',rank(double(A)))])
+
+//adding a homogeneous solid image below the original image , then computing the rank
+//the final rank increases by 1 
+// B=ones(100,299);
+// C=[A;B];
+// imshow(C)
+// A=C;
+// title(['Original (',sprintf('Rank %d)',rank(double(A)))])
